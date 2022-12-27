bf7e47f47737a1cf390980c7991f287eb588912e
Jon Gardeazabal <jon.gardeazabal@opendeusto.es>
2022-12-27 19:46:06 +0100

    First steps with FastAPI

---
+++ app/appComponents.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Owner(BaseModel):
+    dni: str
+    name: str
+    handicap: bool
+
+class Vehicle(BaseModel):
+    registration: str
+    brand: str
+    model: str
+    dni_owner: str
+
+class ParkingLot(BaseModel):
+    id: int
+    registration_vehicle: Optional[str]
+    period: Optional[float]
+
+class Incidence(BaseModel):
+    id: int
+    registration_vehicle: str
+    text: str
