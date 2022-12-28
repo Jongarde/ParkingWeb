9c074898f462a85ede87e8f7bfb18195fa6da8ad
Jon Gardeazabal <jon.gardeazabal@opendeusto.es>
2022-12-28 19:02:34 +0100

    Changes made to the reading of the Vehicle API

--- app/carModels.js
+++ app/carModels.js
@@ -1,15 +1,41 @@
-const request = require('request');
+function getBrands(){
+  const request = require('request');
 
-const options = {
-  url: 'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getModels&make=Ford',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-};
+  const options = {
+    url: 'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getMakes',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
 
-request(options, function(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    const data = JSON.parse(body);
-    console.log(data.Models);
-  }
-});
+  request(options, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      body = body.substring(11, body.length - 3)
+      const data = JSON.parse(body);
+      const brandNames = data.map(item => item.make_display);
+      console.log(brandNames);
+    }
+  });
+}
+
+function getModels(brand){
+  const request = require('request');
+
+  const options = {
+    url: `https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getModels&make=${brand}`,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  request(options, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      body = body.substring(12, body.length - 3)
+      const data = JSON.parse(body);
+      const modelNames = data.map(item => item.model_name);
+      console.log(modelNames);
+    }
+  });
+}
+
+
